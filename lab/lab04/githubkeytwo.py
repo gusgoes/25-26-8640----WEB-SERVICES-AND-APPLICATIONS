ee84a40fb65edc88aac6e148709b27e5a3bcbb86
Gustavo Fernandes <gusgoes@gmail.com>
2026-02-25 20:57:05 +0000

    25022026

--- lab/lab04/githubkeytwo.py
+++ lab/lab04/githubkeytwo.py
@@ -1,5 +1,8 @@
-from github import Github 
+from github import Github
 from config import config as cfg
 apikey = cfg["githubkey"] 
-g = Github(apikey) 
-for repo in g.get_user().get_repos(): print(repo.name)
+g = Github(apikey)
+repo = g.get_repo("yourccount/yourrepo") 
+print(repo.clone_url) 
+fileInfo = repo.get_contents("test.txt") 
+urlOfFile = fileInfo.download_url 
